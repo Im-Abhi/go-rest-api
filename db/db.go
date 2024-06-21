68b5827117993c90d2b14f7b692fa90be73f15ee
Abhishek Gupta <abhishekgupta942001@gmail.com>
2024-06-21 19:59:58 +0530

    creating a sql database table

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -9,12 +10,36 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
+	Db, err := sql.Open("sqlite3", "api.db")
+	DB = Db
 
 	if err != nil {
 		panic("Could not connect to database.")
 	}
 
-	DB.SetMaxOpenConns(10)
-	DB.SetMaxIdleConns(5)
+	Db.SetMaxOpenConns(10)
+	Db.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable :=
+		`
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL,
+			location TEXT NOT NULL,
+			dateTime DATETIME NOT NULL,
+			user_id INTEGER 
+		)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		fmt.Print(err)
+		// panic("Could not create events table")
+	}
 }
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/Im-Abhi/leaning-go/rest-api/db"
 	"github.com/Im-Abhi/leaning-go/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
