72ee07d20c862a54d952b5abf1e8fdb160159aae
Abhishek Gupta <abhishekgupta942001@gmail.com>
2024-06-22 10:56:32 +0530

    Generating JWT

---
+++ utils/jwt.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const SECRET_KEY = "REST-API"
+
+func GenerateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(2 * time.Hour).Unix(),
+	})
+	return token.SignedString(SECRET_KEY)
+}
