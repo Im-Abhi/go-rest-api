bc4badae8f12c96d514cf50b25da0faedbd25f70
Abhishek Gupta <abhishekgupta942001@gmail.com>
2024-06-22 12:55:21 +0530

    Registering users for events

---
+++ routes/registration.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Im-Abhi/leaning-go/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadGateway, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register event."})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "Event registration successful!"})
+}
+
+func cancelRegistration(context *gin.Context) {
+}
